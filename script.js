41bc71fc6eb2011ae05fa0c4a736d92ff0a946ad
Konrad Banasik <konrad2666@gmail.com>
2022-10-28 22:02:12 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// $('.me').on('click', function() {
+//     $('body, html').animate({
+//         scrollTop: $('.s1').offset().top
+//     },500)
+// })
+// $('.portfolio').on('click', function() {
+//     $('body, html').animate({
+//         scrollTop: $('.s2').offset().top
+//     },500)
+// })
+
+$('nav a').on('click', function(){
+   const goToSection = "#" + $(this).attr('class');
+   $('body, html').animate({
+    scrollTop:$(goToSection).offset().top
+   })
+ })
